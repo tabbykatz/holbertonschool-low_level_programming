3507aa61fa87541d724c35b01b64550e0e16ac95
tabbykatz <tomelay@gmail.com>
2020-02-05 19:08:32 +0000

    0x00-hello_world, first pass

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+	
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float)); 
+	return 0;
+}
+
