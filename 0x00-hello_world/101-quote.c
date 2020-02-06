03ea84d7db3991c7d69ca794c6738dc0f6f115d7
tabbykatz <tomelay@gmail.com>
2020-02-06 02:07:13 +0000

    Dora Korpar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 *main - Entry point
 *
-*Return: Always 0 (Success)
+*Return: 1 (Error)
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\"\n", 59);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\"\n", 60);
+	return (1);
 }
