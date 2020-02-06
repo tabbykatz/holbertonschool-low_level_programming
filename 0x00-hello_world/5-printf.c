e72ce357cba4f00b5d87bf67cec0fa09510066d7
tabbykatz <tomelay@gmail.com>
2020-02-06 01:14:02 +0000

    Task 5, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main (void) {
-printf ("with proper grammar, but the outcome is a piece of art,/n");
-return 0;
+/**
+*main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
