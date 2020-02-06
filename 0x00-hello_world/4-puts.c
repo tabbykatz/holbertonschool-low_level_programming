7d32823e19810a36154a7f2a43d94194e5f93b05
tabbykatz <tomelay@gmail.com>
2020-02-06 00:41:43 +0000

    Task 4, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main (void) {
-puts (""Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+puts(""Programming is like building a multilingual puzzle\n");
 return 0;
 }
