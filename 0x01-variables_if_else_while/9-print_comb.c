bee99092da08aed4b83448138a05203ad0b116a3
tabbykatz <tomelay@gmail.com>
2020-02-07 16:23:04 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main- Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+
+	{
+		putchar(x);
+		if (x < 57)
+			putchar(44);
+			putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
+
