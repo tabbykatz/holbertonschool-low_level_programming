ea6067cff9624592a7394fcc4a7b869466f6067a
tabbykatz <tomelay@gmail.com>
2020-02-07 00:53:17 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main- Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
