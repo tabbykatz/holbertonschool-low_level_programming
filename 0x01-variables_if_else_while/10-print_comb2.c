8bd7416f05d1be0064c19ee17eb8f3e276a98769
tabbykatz <tomelay@gmail.com>
2020-02-07 16:43:19 +0000

    Task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main- print all combinations from 0-99
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+		putchar(' ');
+	}
+return (0);
+}
