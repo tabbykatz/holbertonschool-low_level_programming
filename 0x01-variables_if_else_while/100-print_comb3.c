66e3b90e30bffdbf68964c9e23e0a13364393f4b
tabbykatz <tomelay@gmail.com>
2020-02-07 21:21:34 +0000

    Task 11, complete

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  *main- print all combinations from 0-99
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i / 10 < i % 10)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
