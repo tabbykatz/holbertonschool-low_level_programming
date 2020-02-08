f86ddc8e6e369f134c70fef70503fd8f4eb14fc2
tabbykatz <tomelay@gmail.com>
2020-02-08 04:16:17 +0000

    Task 12, first pass

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+  *main- print every combination of 3 digit numbers
+  *without repetition.
+  *
+  *Return- always 0, success
+  *
+  */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			for (z = 0; z < 10; z++)
+			{
+				if (x < y && y < z)
+				{	
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(z + '0');
+					if (x < 7)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					
+				}
+
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
