2f47ed7b1c6324e4f7d54be15838723d7e69313a
tabbykatz <tomelay@gmail.com>
2020-02-08 00:57:52 +0000

    Task 10, betty compliant

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int i;
 
-	for(i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
 		putchar(i / 10 + '0');
 		putchar(i % 10 + '0');
-		if(i < 99)
-		{	
+		if (i < 99)
+		{
 		putchar(44);
 		putchar(32);
 		}
