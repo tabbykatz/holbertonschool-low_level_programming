59a265f1a0302e60821d06534f65c5e2d9672632
tabbykatz <tomelay@gmail.com>
2020-02-08 00:41:45 +0000

    Task 11, complete

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+  *main- a program printing every combination of two digit numbers without repetition
+  *
+  *Return: 0
+  *
+  */
+int main(void)
+{
+int x;/*first*/
+int y;/*second*/
+
+	for (x = 0; x < 100; x ++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (x < y)
+			{
+				putchar('0' + x / 10);
+				putchar('0' + x % 10);
+				putchar(32);
+				putchar('0' + y / 10);
+				putchar('0' + y % 10);
+				if (x !=98 || y != 99)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}	
+
