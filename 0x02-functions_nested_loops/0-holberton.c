0d8fbd0f04f0c7c8bcd1f5b899c86c95a8730224
Tabitha O'Melay <tomelay@gmail.com>
2020-02-10 22:40:22 +0000

    Adding holberton.h

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *main- a program that prints Holberton
+  *
+  *return: zero
+  *
+  */
+int main(void)
+{
+char Holberton[9] = "Holberton";
+int counter;
+for (counter = 0; counter < 10; counter++)
+	{
+		_putchar(Holberton[counter]);
+	}
+_putchar('\n');
+return 0;
+}
