41d6f9165c03824353fe79c7a82b7b335b3fc968
Tabitha O'Melay <tomelay@gmail.com>
2020-02-11 02:53:44 +0000

    Task 3, first pass

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+  * main - entry point
+  *
+  * Return: zero on success
+  *
+  */
+int main(void)
+{
+_islower(int);
+return (0);
+}
+/**
+  * _islower - perceives whether a character is lower case
+  *
+  * Return: 1 if lower, 0 if other
+  */
+int _islower(int)
+{
+int c;
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
