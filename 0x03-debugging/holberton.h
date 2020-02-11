6d6f138d940cc58fe669e1653230a0596adfcd09
Tabitha O'Melay <tomelay@gmail.com>
2020-02-11 21:21:38 +0000

    README and header

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,16 @@
+  1 #ifndef HOLBERTON
+  2 #define HOLBERTON
+  3 int _putchar(char c);
+  4 void print_alphabet(void);
+  5 void print_alphabet_x10(void);
+  6 int _islower(int c);
+  7 int _isalpha(int c);
+  8 int print_sign(int n);
+  9 int _abs(int);
+ 10 int print_last_digit(int);
+ 11 void jack_bauer(void);
+ 12 void times_table(void);
+ 13 int add(int, int);
+ 14 void print_to_98(int n);
+ 15 void print_times_table(int n);
+ 16 #endif /*HOLBERTON*/
