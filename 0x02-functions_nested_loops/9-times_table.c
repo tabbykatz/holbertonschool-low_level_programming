f6dacc48544d70e56b623a64b4918e490bf27c15
Tabitha O'Melay <tomelay@gmail.com>
2020-02-11 18:36:46 +0000

    Task 9, times tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+  * times_table prints the 9 times table
+  *
+  * Return: void
+  *
+  */
+void times_table(void)
+{
+int x;
+int y;
+int product;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			product = x * y;
+			if (product < 10)
+			{
+				if (y != 0)
+					_putchar(' ');
+				_putchar(product + '0');
+				if (y <= 8)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+				if (y <= 8)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+	_putchar('\n');
+	}
+}
