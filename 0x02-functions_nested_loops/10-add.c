b2fde98db70f692404d8bc4a0b13bd04c3575226
Tabitha O'Melay <tomelay@gmail.com>
2020-02-11 18:47:31 +0000

    Task 10, first pass

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+  * add- adds two integers and returns the result
+  * @a: one of the #s
+  * @b: the other #
+  * Return:  result
+  */
+int add(int a, int b)
+{
+return (a + b);
+}
