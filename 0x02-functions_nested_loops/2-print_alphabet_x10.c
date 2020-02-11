eb0530628354e513a8969851852035655e18ac27
Tabitha O'Melay <tomelay@gmail.com>
2020-02-11 15:45:13 +0000

    Task 2, removed main

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,5 @@
 #include "holberton.h"
 /**
-  * main- entry point
-  *
-  * Return: 0
-  *
-  */
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
-/**
   * print_alphabet_x10- prints the alphabet 10x
   *
   * Return ()
