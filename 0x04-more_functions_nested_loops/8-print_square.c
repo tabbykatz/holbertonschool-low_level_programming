9a8a2e827d5dc485134096b736ae3242d99124d3
Tabitha O'Melay <tomelay@gmail.com>
2020-02-13 20:16:06 +0000

    Task 8, first pass

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * print_square - prints a hashtag square
+  * @size: how big the square is
+  *
+  *
+  * Return: void
+  */
+void print_square(int size)
+{
+int x;
+int y;
+
+if (size >= 0)
+{
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
+}
