d50aba003f404cc433ac199cd36d26b149330570
Tabitha O'Melay <tomelay@gmail.com>
2020-02-13 20:01:44 +0000

    Task 5, up to 14 this time

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,13 @@ int i;
 int n;
 for (i = 0; i < 10; i++)
 {
-	for (n = 0; n < 10; n++)
+	for (n = 0; n < 15; n++)
 	{
-		_putchar(n + '0');
+		if (n > 10)
+		{
+			_putchar(n  / 10 + '0');
+		}
+		_putchar(n % 10 + '0');
 	}
 _putchar('\n');
 }
