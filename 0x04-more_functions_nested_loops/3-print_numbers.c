1561855b3083690d24d81544a94149d67fc1e976
Tabitha O'Melay <tomelay@gmail.com>
2020-02-13 18:34:01 +0000

    Task 3, first pass

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  * print_numbers - prints numbers 0-9
+  *
+  *
+  * Return: void
+  */
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
+}
+
