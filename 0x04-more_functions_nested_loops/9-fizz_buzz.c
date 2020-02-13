700ef94984c678f8f8ee6d7c40ecd435478eadd1
Tabitha O'Melay <tomelay@gmail.com>
2020-02-13 20:39:53 +0000

    Task 9, brought the spaces back

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,10 +19,9 @@ for (num = 1; num < 101; num++)
 	else if (num % 3 == 0 && num % 5 == 0)
 		printf("FizzBuzz");
 	else
-	{	
+}
 		printf("%d", num);
 	}
-	if (num != 100)
 		printf(" ");
 }
 printf("\n");
