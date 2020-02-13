387e13e9fa0f13c4847a54ec4538bf7c3edfb205
Tabitha O'Melay <tomelay@gmail.com>
2020-02-13 18:44:52 +0000

    Task 4, first pass

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  * print_most_numbers - prints all the numbers except 2 and 4
+  *
+  *
+  * Return: void
+  */
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	if (i != 2 && i != 4)
+		_putchar(i + '0');
+
+}
+_putchar('\n');
+}
