3ed4cc5d5435af3c6e5ac53506de7098687eb523
Tabitha O'Melay <tomelay@gmail.com>
2020-02-13 04:11:23 +0000

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * main - fibbonacci
+  *
+  *
+  * Return: 0
+  */
+int main(void)
+{
+long int a, b, c;
+long int i;
+int upto;
+a = 0;
+b = 1;
+c = 1;
+upto = 100;
+for (i = 1; i <= upto; i++)
+{
+
+	printf("%li", c);
+	a = b;
+	b = c;
+	c = a + b;
+if (i < 100)
+	printf(", ");
+}
+printf("\n");
+return (0);
+}
