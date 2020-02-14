2d4c4632faff700e3b7ea0d1589ea9e42c0d4130
Tabitha O'Melay <tomelay@gmail.com>
2020-02-14 04:20:33 +0000

    Task 12, only the beginning... not working

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,24 +17,17 @@ _putchar('-');
 n = -n;
 }
 for (l = 0; l <= n; l++)
-{
-	printf("the value of l is %d\n", l);
-	
+{	
 	if ( n == 0)
 	{
 		_putchar('0');
 		break;
-		printf("this is the zero if\n");
 	}
 	else
-		for (l = 0)
 		_putchar(n % 10);
 		last = n % 10;
-		printf("The value of last is %d\n", last);
 		n = n - last;
-		printf("The value of n is %d\n", n);
 		n = n / 10;
-		printf("Now the value of n is %d\n", n);
 	}
 _putchar('\n');
 }
