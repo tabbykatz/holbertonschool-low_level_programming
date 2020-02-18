725caf421a572e7cb62e39b5e3454809753be7ad
Tabitha O'Melay <tomelay@gmail.com>
2020-02-18 18:24:14 +0000

    Task 5, reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 /**
   * rev_string - reverse a string
-  * @: that's the string
+  * @s: that's the string
   *
   * Return: void)
   *
   */
 void rev_string(char *s)
 {
-char f[1];
-int i = 0;
-int l = 0;
-while (*s)
-{
-	s++;
-	l++;
-}
-while (l)
-{
-	s[i] = f[0];
-	s[l] = s[i];
-	s[i] = f[0];
-	l--;
-	i++;
-}
+	char *start = s;
+	char temp;
+
+	for (; *(s + 1); )
+	{
+		s++;
+	}
+	while (start < s)
+	{
+		temp = *start;
+		*start = *s;
+		*s = temp;
+		s--;
+		start++;
+	}
+
 }
