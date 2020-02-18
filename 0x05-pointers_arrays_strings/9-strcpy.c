86fed8ed1d8de369a91758e349eb4ebde638799e
Tabitha O'Melay <tomelay@gmail.com>
2020-02-18 22:23:45 +0000

    Task 9, returning the answer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,16 @@
   */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
+	char *answer = dest;
+	int i = 0;
+
+	for (; *src; i++)
 	{
 		*dest = *src;
 		dest++;
 		src++;
 	}
-return (dest);
+	dest[i] = '\0';
+return (answer);
 }
 
