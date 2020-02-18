f4027b908f063b327ddc03e983f271d2f696e0cb
Tabitha O'Melay <tomelay@gmail.com>
2020-02-18 18:27:03 +0000

    Task 0, betty compliant

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
+  * reset_to_98 - changing the value
   *
-  *
-  *
-  *
+  * @n: that's the pointer
+  * Return: void
   */
 void reset_to_98(int *n)
-{ 
+{
 
-*n = 98; 
+*n = 98;
 }
