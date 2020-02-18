f734a831113b1deca8d267bf9f0d9cbf04de1048
Tabitha O'Melay <tomelay@gmail.com>
2020-02-18 20:33:31 +0000

    Task 7, printing half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+  * puts_half - printing half a string
+  * @str: that's the string
+  *
+  * Return: void
+  */
+void puts_half(char *str)
+{
+	int length = 0;
+	char *start = str;
+
+	while (*(str))
+	{
+		length++;
+		str++;
+	}
+	length += 1;
+	if (length / 2 == 0)
+	{
+		while (*start)
+		{
+			_putchar(*(start + (length / 2)));
+			start++;
+		}
+	}
+	else
+	{
+		while (*start)
+		{
+			_putchar(*(start + ((length - 1) / 2)));
+			start++;
+		}
+	}
+	_putchar('\n');
+}
