72a7bb323b40ac121cbf2d55419eec86d1f6ea41
Tabitha O'Melay <tomelay@gmail.com>
2020-02-20 19:18:09 +0000

    Task 3, first pass

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+  * _strcmp - a function to compare 2 strings
+  * @s1: the first string
+  * @s2: the second string
+  *
+  * Return: int
+  */
+int _strcmp(char *s1, char *s2)
+{
+	return (s1[0] - s2[0]);
+}
