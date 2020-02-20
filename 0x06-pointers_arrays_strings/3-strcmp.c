2a1cbf71be0b00ec60559444c141a66f4a1af00f
Tabitha O'Melay <tomelay@gmail.com>
2020-02-20 21:49:36 +0000

    Task 3, no variable iteration

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,5 +8,15 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	return (s1[0] - s2[0]);
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+		break;
+	}
+	return (*s1 - *s2);
 }
