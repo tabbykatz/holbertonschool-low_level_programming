4cf8f762bdb1086a056fbb1a7dccb971cc922798
Tabitha O'Melay <tomelay@gmail.com>
2020-02-24 21:40:09 +0000

    Task 2, return *s even if c is NULL

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,16 @@
   */
 char *_strchr(char *s, char c)
 {
+	if (s == NULL)
+		return (NULL);
 	for (; *s; s++)
 	{
 		if (*s == c)
 			return (s);
+	}
+	if (*s == c)
+	{
+		return (s);
 	}
 	return (NULL);
 }
