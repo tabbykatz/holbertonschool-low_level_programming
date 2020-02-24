f6b14b7535cdb7398cf65fbe249f1545eb839cdd
Tabitha O'Melay <tomelay@gmail.com>
2020-02-24 21:01:46 +0000

    Task 3,  array notation

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,20 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
+	int i = 0;
 	int j = 0;
-	unsigned int count = 0;
 
-	for (; *s; s++)
+	for (; s[i]; i++)
 	{
-		for (j = 0; *accept; j++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (*s == *(accept + j))
+			if (s[i] == accept[j])
 			{
-				count++;
-				s++;
 				break;
 			}
-			else if (*(accept + j) == '\0')
-				break;
 		}
+			if (s[i] != accept[j])
+			break;
 	}
-	return (count);
+	return (i);
 }
