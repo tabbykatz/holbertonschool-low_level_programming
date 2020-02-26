19904e403895426afb9a0d4427c7fc7d1a7a7d0b
Tabitha O'Melay <tomelay@gmail.com>
2020-02-26 17:35:26 +0000

    Task 2, first pass

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
-
 /**
- * _strlen_recursion -
- *
- * Return: 
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: int/ length of string
  */
 int _strlen_recursion(char *s)
 {
+	int len = 0;
 
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(++s);
+		return (len);
+	}
+	return (0);
 }
