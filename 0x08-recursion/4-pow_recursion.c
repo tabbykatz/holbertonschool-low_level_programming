4304fda320ee14727e2f71c275c48a5235404eeb
Tabitha O'Melay <tomelay@gmail.com>
2020-02-26 17:49:54 +0000

    Task 4, first pass

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
-
 /**
- * _pow_recursion -
- *
- * Return: 
+ * _pow_recursion - returns x to the y
+ * @x: one num
+ * @y: the exponent
+ * Return: x to the y or -1
  */
 int _pow_recursion(int x, int y)
 {
-
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else if (y % 2 == 0)
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+	else
+		return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 }
