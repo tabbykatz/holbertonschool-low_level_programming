9a48e3943d82dfc54f7868b01fbc8b781531fa09
Tabitha O'Melay <tomelay@gmail.com>
2020-02-26 17:40:56 +0000

    Task 3, first pass

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
-
 /**
- * factorial -
- *
- * Return: 
+ * factorial - returns factorial of n
+ * @n: the number
+ * Return: 1, -1, or factorial
  */
 int factorial(int n)
 {
-
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
 }
