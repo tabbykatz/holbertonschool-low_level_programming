3dedbef5c8835d50465dc6217f0e9fb1c142e4ca
Tabitha O'Melay <tomelay@gmail.com>
2020-02-26 17:12:11 +0000

    Task 1, first pass

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,16 @@
 #include "holberton.h"
-
 /**
- * _print_rev_recursion -
- *
- * Return: 
+ * _print_rev_recursion - print a string in reverse
+ * @s: the string
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
-
+	if (!*s)
+		return;
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
