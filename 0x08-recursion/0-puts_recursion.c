d9baaac01ad7c11074e3ed81f4839c4632576f4d
Tabitha O'Melay <tomelay@gmail.com>
2020-02-26 17:02:10 +0000

    Task 0, first pass

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
-
 /**
- * _puts_recursion -
- *
- * Return: 
+ * _puts_recursion - prints a string followed by a newline
+ * @s: the string we print
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-
+	if (!*s)
+		return;
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+		if (*(s + 1) == '\0')
+			_putchar('\n');
+	}
 }
