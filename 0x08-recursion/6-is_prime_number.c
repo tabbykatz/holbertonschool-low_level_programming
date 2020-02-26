96dd735309bd3332138c16d6c98027991138365b
Tabitha O'Melay <tomelay@gmail.com>
2020-02-26 21:10:32 +0000

    Task 6, first pass

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,37 @@
 #include "holberton.h"
-
 /**
- * is_prime_number -
- *
- * Return: 
+  * _isitprime - looks for prime
+  * @n: the number
+  * @i: the number / 2
+  *
+  * Return: 1 if prime, 0 if not
+  */
+int _isitprime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (_isitprime(n, i - 1));
+	}
+}
+/**
+ * is_prime_number - looks for prime
+ * @n: the number
+ * Return: 1 if orime, 0 if not
  */
 int is_prime_number(int n)
 {
+	if (n < 2)
+		return (0);
+	if (n % 2 == 0)
+		return (0);
+	else
+		return (_isitprime(n, n / 2));
 
 }
