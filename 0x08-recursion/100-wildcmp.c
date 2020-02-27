b9a852b4998997589dfb7c35029b193c7c4be5a9
Tabitha O'Melay <tomelay@gmail.com>
2020-02-27 03:04:31 +0000

    Task 8, additional brackets

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -8,15 +8,23 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == *s2 && *s1 && *s2)
+	{
 		return (wildcmp(s1 + 1, s2 + 1));
+	}
 	if (!*s1 && !*s2)
+	{
 		return (1);
+	}
 	if (*s2 == '*')
 	{
 		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+		{
 			return (1);
+		}
 		if (!*(s1 + 1) && !*s2)
+		{
 			return (0);
+		}
 	}
 	return (0);
 }
