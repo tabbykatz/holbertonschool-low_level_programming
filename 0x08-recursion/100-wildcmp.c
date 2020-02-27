25bd7665ca25075d66a9ed0c6edb345b4f9c387e
Tabitha O'Melay <tomelay@gmail.com>
2020-02-27 04:26:21 +0000

    Task 8, messy changes

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -8,21 +8,23 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == *s2 && *s1 && *s2)
-	{
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
 	if (!*s1 && !*s2)
-	{
 		return (1);
-	}
 	if (*s2 == '*')
 	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+		if (*(s2 + 1) != *s1 && *(s2 + 1) != '*')
 		{
-			return (1);
+			return (wildcmp(s1 + 1, s2));
 		}
-		if (!*(s1 + 1) && !*(s2 + 1))
-			return (0);
+		else if (*(s2 + 1) != *s1 && *(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		if (!wildcmp(s1, s2 + 1))
+			return (wildcmp(s1 + 1, s2));
+		if (wildcmp(s1, s2 + 1))
+			return (1);
 	}
 	return (0);
 }
