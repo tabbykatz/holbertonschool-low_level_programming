d51149d6026603bdb2506de9a7b5bfad15d6b7c6
Tabitha O'Melay <tomelay@gmail.com>
2020-02-27 01:50:11 +0000

    Task 8, first pass

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
-
 /**
- * wildcmp -
- *
- * Return: 
+ * wildcmp - checks two strings for match even if one is full of wildcards
+ * @s1: one string
+ * @s2: the string with *
+ * Return: 1 if match, otherwise 0
  */
 int wildcmp(char *s1, char *s2)
 {
-
+	if (*s1 == *s2 && *s1 && *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+		if (!*(s1 + 1) && !*s2)
+			return (0);
+	}
+	return (0);
 }
