4eabeb7b4fa79b1cd6d692f3ae9627d21a401a3c
Tabitha O'Melay <tomelay@gmail.com>
2020-03-03 21:28:59 +0000

    Task 0, checking for malloc fail

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	array = malloc(size * sizeof(char));
-
 	if (size == 0)
 		return (NULL);
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
