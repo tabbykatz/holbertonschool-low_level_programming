0f2a7c936a7fcf4d9dcd7ef48ce8876655d4104c
Tabitha O'Melay <tomelay@gmail.com>
2020-03-03 22:03:53 +0000

    task 4, first pass

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
 /**
- * free_grid -
- *
- * Return: 
+ * free_grid - free a 2d grid
+ * @grid: the grid
+ * @height: the height
+ * Return: void
  */
 void free_grid(int **grid, int height)
 {
+	int i;
 
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
