5e30568e089b77b7e475ef6157a740ba4b7a7e78
Tabitha O'Melay <tomelay@gmail.com>
2020-03-04 03:18:55 +0000

    Task 5, concatenating

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,11 +1,58 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * argstostr -
- *
- * Return: 
+ * _strlen - length of a string
+ * @s: the string
+ * Return: length
+ **/
+int _strlen(char *s)
+{
+	int len;
+
+	if (!s)
+		return (0);
+	for (len = 0; s[len]; len++)
+	{
+	}
+	return (len);
+}
+/**
+ * argstostr - concasts args to str
+ * @ac: # of args
+ * @av: names of args
+ * Return: char *
  */
 char *argstostr(int ac, char **av)
 {
+	int i;
+	int j;
+	int x = 0;
+	int len = 0;
+	char *newstring;
 
+	if (!ac || !av)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(*(av + i));
+	}
+	newstring = malloc(sizeof(char) * (len + ac + 1));
+	if (!newstring)
+	{
+		return (NULL);
+	}
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++, x++)
+		{
+			newstring[x] = av[i][j];
+		}
+		newstring[x++] = '\n';
+	}
+	newstring[x] = '\0';
+	return (newstring);
 }
