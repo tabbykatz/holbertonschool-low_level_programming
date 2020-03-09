0776677b5e3bdf3b0f9ec012cab51ce7b46d4ab6
Tabitha O'Melay <tomelay@gmail.com>
2020-03-09 22:17:01 +0000

    Task 2, not age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,18 @@
 #include "dog.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * new_dog -
- *
- * Return: 
+ * print_dog - prints the struct dog
+ * @d: points to dog
+ * Return: void
  */
-dog_t *new_dog(char *name, float age, char *owner)
+void print_dog(struct dog *d)
 {
-
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = ("nil");
+	if (d->owner == NULL)
+		d->owner = ("nil");
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
