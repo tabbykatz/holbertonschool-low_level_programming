05770e68b485522a3b56751b99c228db13d7b86e
Tabitha O'Melay <tomelay@gmail.com>
2020-03-09 18:28:18 +0000

    Task 2, first pass

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - checking to see if this prints name of file
+  *
+  * Return: 0
+  *
+  *
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+
