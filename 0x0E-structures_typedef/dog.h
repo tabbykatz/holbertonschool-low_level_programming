55308dfaa31dd811f34e4058ebd4508073c9367d
Tabitha O'Melay <tomelay@gmail.com>
2020-03-09 22:23:33 +0000

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+}dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 void print_dog(struct dog *d);
