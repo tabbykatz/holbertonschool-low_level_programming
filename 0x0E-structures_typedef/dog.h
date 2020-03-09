5328cafa6e6baad2e6c6f89399360d3e81065d83
Tabitha O'Melay <tomelay@gmail.com>
2020-03-09 20:41:37 +0000

    Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,10 +8,14 @@
  *
  * Description: just keeping track of the pups
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /*DOG_H */
