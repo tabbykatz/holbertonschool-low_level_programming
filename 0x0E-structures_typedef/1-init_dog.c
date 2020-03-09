8735dede106b265b21841032b96bf1e04cf7a88c
Tabitha O'Melay <tomelay@gmail.com>
2020-03-09 20:37:54 +0000

    Task 1, first pass

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,19 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * print_dog -
- *
- * Return: 
+ * init_dog - keeping track of the pups
+ * @d: points to dog
+ * @name: dog name
+ * @age: age of dog
+ * @owner: name of dog's owner
+ * Return: void
  */
-void print_dog(struct dog *d)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
