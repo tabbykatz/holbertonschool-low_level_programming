99a43fe5d3252d6fe6a8ede3955cb98ec26bb334
Tabitha O'Melay <tomelay@gmail.com>
2020-03-09 23:04:16 +0000

    Task 2, moved quotes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d->name = ("nil");
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = ("nil");
+		d->owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
