10cb38969ea39000825bcef51f418daf62a02eef
Tabitha O'Melay <tomelay@gmail.com>
2020-03-11 19:54:52 +0000

    Task 2, checking for NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,10 +9,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	/* declarations */
 	int i;
 
+	/* checking input */
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
+	/* moving through array looking for int */
 	for (i = 0; i < size; i++)
 	{
 		if ((*cmp)(array[i]))
