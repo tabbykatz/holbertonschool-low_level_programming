684a1df3ca41da2f41c4c8667dd6f74433f5f30c
Tabitha O'Melay <tomelay@gmail.com>
2020-03-11 20:47:15 +0000

    Task 4, opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,31 @@
-#include "function_pointers.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * op_sub -
- *
- * Return: 
+ * main - entry
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: int
  */
-int op_sub(int a, int b)
+int main(int argc, char **argv)
 {
+	/* declarations */
+	int i;
+	unsigned char *pointer = (char *) main;
+	/* checking input */
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("error\n");
+		exit(2);
+	}
 
+	/* prints opcodes */
+	for (i = 0; i < (atoi(argv[1]) - 1); i++, pointer++)
+		printf("%02x ", *pointer);
+	printf("%02x\n", *pointer);
+	return (0);
 }
