9b913c8b5ef8aa5b04f82a45af3522ebd49011a0
Tabitha O'Melay <tomelay@gmail.com>
2020-03-11 19:04:24 +0000

    Task 3, setup

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,24 @@
-#include "function_pointers.h"
-
+#include "3-calc.h"
 /**
- * op_add -
- *
- * Return: 
- */
-int op_add(int a, int b)
+  * main - entry point
+  * @argc: # of args
+  * @argv: array of pointers to names of args
+  * Return: 0
+  */
+int main(int argc, char **argv)
 {
-
+	/* input checking */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/* calling get_op_func with arguments */
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+  * op_add - adds stuff
+  * @a: one int
+  * @b: the other int
+  * Return: int
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - subtracts stuff
+  * @a: one int
+  * @b: the other int
+  * Return: int
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * op_mul - multiplies ints
+  * @a: one int
+  * @b: the other int
+  * Return: int
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - divides ints
+  * @a: one int
+  * @b: the other int
+  * Return: int
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - modulo
+  * @a: one int
+  * @b: the other int
+  * Return: int
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
