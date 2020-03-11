3ccf6a45a297f7fdffb590111d06dd6d8751ce59
Tabitha O'Melay <tomelay@gmail.com>
2020-03-11 17:13:33 +0000

    Task 1 first pass

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,11 +1,18 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
- *
- * Return: 
+ * array_iterator - executes a function on each element of arr
+ * @array: the arr
+ * @size: size of array?
+ * @action: the function to execute
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
 
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
 }
