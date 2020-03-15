92ba87cc756883db108afca45e8bc7efd3e26351
tabbykatz <tomelay@gmail.com>
2020-03-15 03:07:37 +0000

    Task 4, main instead of start

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,24 @@
+%define sys_write 1
+%define stdout 1
+
+%define sys_exit 60
+%define success 0
+
+%define nl 10
+
 section .data
 
-    message db "Hello, Holberton", 10
+    message db "Hello, Holberton", nl
+    message_len equ $-message
 
 section .text
-
-global _start
-_start:
-	mov rax, 1
-    	mov rdi, 1
+global main
+main:
+	mov rax, sys_write
+    	mov rdi, stdout
     	mov rsi, message
 	mov rdx, 17
     	syscall
-        mov rax, 60
-	mov rdi, 0
+        mov rax, sys_exit
+	mov rdi, success
 	syscall
