a0ccf7f11d7ccf88d1a3af182552e2187cb93db2
tabbykatz <tomelay@gmail.com>
2020-03-19 21:10:14 +0000

    removed some brackets

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_list - prnt all the elements of list_t
+ * print_list - print all the elements of list_t
  * @h: the head of the list
  * Return: number of nodes
  */
@@ -17,9 +17,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		nodes += 1;
 		h = h->next;
 	}
