eb937d7fbbf93972595c1735c5d618eb04d512fd
tabbykatz <tomelay@gmail.com>
2020-03-23 16:58:30 +0000

    Task 5, free the list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
- *
- * Return: 
+ * free_listint2 - frees the list and sets head to NULL
+ * @head: double ptr to list
+ * Return: void
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *location;
+	listint_t *phree;
 
+	if (!head)
+		return;
+	location = *head;
+
+	while (location)
+	{
+		phree = location;
+		location = location->next;
+		free(phree);
+	}
+	*head = NULL;
 }
