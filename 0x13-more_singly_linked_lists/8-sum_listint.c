e75c80d9d024d2bacee6cc8ba1e3c34729232d90
tabbykatz <tomelay@gmail.com>
2020-03-23 18:17:17 +0000

    Task 8, return sum of data

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_listint -
- *
- * Return: 
+ * sum_listint - returns sum of all data in list
+ * @head: points to list
+ * Return: sum or 0
  */
 int sum_listint(listint_t *head)
 {
+	int sum = 0;
 
+	if (!head)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
