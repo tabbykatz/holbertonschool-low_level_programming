680a05d6136841bc8a3957d72465a22d8ca74472
tabbykatz <tomelay@gmail.com>
2020-03-23 18:13:11 +0000

    Task 7, return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,23 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -
- *
- * Return: 
+ * get_nodeint_at_index - returns the nth node of list
+ * @head: points to list
+ * @index: which node
+ * Return: node in question or NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
 
+	if (!head)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		if (head->next)
+		head = head->next;
+		else
+			return (NULL);
+	}
+	return (head);
 }
