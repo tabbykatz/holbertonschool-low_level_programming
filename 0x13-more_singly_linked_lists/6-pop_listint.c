1fadb17bab4000d467b1ccd8e7e7dec3ac574fdc
tabbykatz <tomelay@gmail.com>
2020-03-23 17:51:53 +0000

    Task 6, pop off the first node & return it's data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * pop_listint -
- *
- * Return: 
+ * pop_listint - deletes the head node of list & returns data(n)
+ * @head: points to list
+ * Return: int n
  */
 int pop_listint(listint_t **head)
 {
+	/* declarations */
+	listint_t *location;
+	int data = 0;
 
+	/* check for null pointer */
+	if (!(*head))
+		return (0);
+	/* capture n */
+	data += (*head)->n;
+	/* insulate head & move head pointer to new beginning */
+	location = *head;
+	*head = (*head)->next;
+	free(location);
+	return (data);
 }
