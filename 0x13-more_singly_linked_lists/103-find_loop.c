5926e4029deec53c2c9448a3240619a8d929fd6d
tabbykatz <tomelay@gmail.com>
2020-03-24 21:30:09 +0000

    find a loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,11 +1,22 @@
 #include "lists.h"
 
 /**
- * find_listint_loop -
- *
- * Return: 
+ * find_listint_loop - fnd a loop in a list
+ * @head: points to beginning of list
+ * Return: the address where loop starts or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-
+	if (!head)
+		return (NULL);
+	while (head)
+	{
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
+			return (head);
+	}
+	return (NULL);
 }
