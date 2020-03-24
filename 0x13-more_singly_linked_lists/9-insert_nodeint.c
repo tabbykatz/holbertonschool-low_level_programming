031c809d91bebd951a197feb4b694e404864b52d
tabbykatz <tomelay@gmail.com>
2020-03-24 17:52:09 +0000

    insert at index, while location

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-	for (i = 0; i < (idx - 1) && location->next; i++)
+	for (i = 0; i < (idx - 1) && location; i++)
 	{
 		location = location->next;
 	}
