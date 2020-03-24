1cfb8217f008d50902d5e38ae8899cdd11f76d7a
tabbykatz <tomelay@gmail.com>
2020-03-24 18:13:11 +0000

    delete the correct node of index == 0

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,28 +10,28 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *location;
-	listint_t *temp;
+	listint_t *temp = NULL;
 
 	if (!(head && *head))
 		return (-1);
 	location = *head;
-	for (i = 0; i < (index - 1); i++)
-	{
-		if (location->next)
-			location = location->next;
-		else
-			return (-1);
-	}
 	if (index == 0)
 	{
 		temp = location;
 		*head = location->next;
+		free(temp);
+		return (1);
 	}
 	else
-	{
+		for (i = 0; i < (index - 1); i++)
+		{
+			if (location->next)
+			location = location->next;
+			else
+				return (-1);
+		}
 		temp = location->next;
 		location->next = temp->next;
-	}
 	free(temp);
 	return (1);
 }
