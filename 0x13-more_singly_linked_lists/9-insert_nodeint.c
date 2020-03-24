9d40d18237763f0f49bc6eb186bd775c99ecca31
tabbykatz <tomelay@gmail.com>
2020-03-24 18:27:25 +0000

    insert node even if inx is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,13 +14,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	unsigned int i;
 
-	if (!(*head))
+	if (!*head)
 		return (NULL);
 	location = *head;
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next =  location;
+		*head = new;
+		return (*head);
+	}
 	for (i = 0; i < (idx - 1) && location; i++)
 	{
 		location = location->next;
