7df5ef3365f0eb5523d9afb67f0e5bfc28e82fc9
tabbykatz <tomelay@gmail.com>
2020-03-24 21:21:06 +0000

    free list, avoid loop

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * free_listint_safe -
- *
- * Return: 
+ * free_listint_safe - frees a list
+ * @h: the pointer to the start of list to free
+ * Return: size of free'd list (in nodes?)
  */
 size_t free_listint_safe(listint_t **h)
 {
+	size_t nodeCount = 0;
+	listint_t *location;
+	listint_t *temp = NULL;
 
+	if (!h)
+		return (0);
+	for (; location; nodeCount++)
+	{
+		if (location < location->next)
+		temp = location;
+		location = location->next;
+		free(temp);
+	}
+	*h = NULL;
+	return (nodeCount);
 }
