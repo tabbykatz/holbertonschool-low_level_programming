0a464c7d7fa6ca52a55f278b7ab8be34e4347e66
tabbykatz <tomelay@gmail.com>
2020-03-25 01:09:50 +0000

    prints the list, with a reversed if

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,21 +10,21 @@ size_t print_listint_safe(const listint_t *head)
 	/* declarations */
 	size_t nodeCount = 0;
 	/* check for null pointer */
-	if (!head)
-		exit(98);
 	while (head)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		nodeCount += 1;
 
-		if (head < head->next)
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
 		{
 			head = head->next;
 			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
+			break;
 		}
-		else
-			head = head->next;
 	}
 	return (nodeCount);
 }
