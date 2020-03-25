6af4fa40572b27c114a0131aa592e5f4267dbc06
tabbykatz <tomelay@gmail.com>
2020-03-25 01:41:37 +0000

    free the list, now with proper inequality

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,17 +8,24 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t nodeCount = 0;
-	listint_t *location;
 	listint_t *temp = NULL;
 
-	if (!h)
-		return (0);
-	for (; location; nodeCount++)
+	if (!(h && *h))
+		return (nodeCount);
+	while (*h)
 	{
-		if (location < location->next)
-		temp = location;
-		location = location->next;
-		free(temp);
+		nodeCount++;
+		if (*h > (*h)->next)
+		{
+			temp = *h;
+			*h = (*h)->next;
+			free(temp);
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+		}
 	}
 	*h = NULL;
 	return (nodeCount);
