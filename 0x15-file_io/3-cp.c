e4f4c4c11843625717c848f862990a0d47537e85
tabbykatz <tomelay@gmail.com>
2020-03-31 19:48:15 +0000

    Task 3, added exit status

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,31 +27,33 @@ int main(int argc, char **argv)
 		printerrors("Usage: cp file_from file_to", "", 97);
 
 	fddest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fddest < 0)
+	if (fddest == -1)
 		printerrors("Error: Can't write to ", argv[2], 99);
 
 	fdsrc = open(argv[1], O_RDONLY);
-	if (fdsrc < 0)
+	if (fdsrc == -1)
 		printerrors("Error: Can't read from file ", argv[1], 98);
 
 
 	do {
 		readVal = read(fdsrc, buffer, 1024);
-		if (readVal < 0)
+		if (readVal == -1)
 			printerrors("Error: Can't read from file ", argv[1], 98);
 
 		writeVal = write(fddest, buffer, readVal);
-		if (writeVal < 0 || writeVal != readVal)
+		if (writeVal == -1 || writeVal != readVal)
 			printerrors("Error: Can't write to ", argv[2], 99);
 
 		} while (writeVal == 1024);
 	if (close(fdsrc))
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdsrc);
+		exit(100);
 	}
 	if (close(fddest))
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fddest);
+		exit(100);
 	}
 	return (0);
 }
