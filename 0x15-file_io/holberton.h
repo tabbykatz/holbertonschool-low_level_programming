ea69dfd301fc76e73496a984690b19b87a2e65b0
tabbykatz <tomelay@gmail.com>
2020-03-31 19:19:32 +0000

    header update

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
