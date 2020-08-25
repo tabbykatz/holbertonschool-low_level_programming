9763bf90858e65b3c2d47abe11fa2dd4e35d80c4
tabbykatz <tomelay@gmail.com>
2020-08-25 20:16:57 +0000

    Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+/**
+ * linear_search- look for avlue sequentially
+ * @array: the array of values
+ * @size: size of array
+ * @value: what we are searching for
+ * Return: index found at or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index = 0;
+
+	if (!array)
+		return (-1);
+	while (index <= size)
+	{
+		if (value == array[index])
+			return (index);
+		index++;
+	}
+	return (-1);
+}
